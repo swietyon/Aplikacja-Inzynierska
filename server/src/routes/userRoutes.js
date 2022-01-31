4ee99ac6a160b6991c30406008956e2709942201
swietyon <32486978+swietyon@users.noreply.github.com>
2022-01-31 12:12:35 +0100

    update

--- server/src/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express=require('express'); 
-const UserController=require('../controllers/userController'); 
- 
-const router=express.Router(); 
- 
-module.exports=router.get('/Users',UserController.getUsers);
